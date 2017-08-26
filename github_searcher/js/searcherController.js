eefc059ba5713cc3d906331ecfb419c661baece8
Elatha <bernat.90@gmail.com>
2017-08-26 20:02:14 +0200

    AJAX with the api to search by repository name

--- github_searcher/js/searcherController.js
+++ github_searcher/js/searcherController.js
@@ -3,8 +3,47 @@
 //setup the search of github repositories
 
 
+setupSearcher(".search.form", (sectionDiv) => {
+
+    const searchTerm = document.querySelector("input").value;
+
+    const url = 'https://api.github.com/search/repositories?q=' + searchTerm;
+
+    displayResult(sectionDiv, "sending request with fetch...");
+
+    fetch(url)
+        .then(x => x.json()) // gets response as JSON object, so it does JSON.parse() for us
+        .then(data => {
+            console.log("AJAX request finished correctly :)");
+            console.log(data);
+            const result = generateList(data);
+            displayResult(sectionDiv, result);
+        })
+        .catch(error => {
+            console.log("AJAX request finished with an error :(");
+            displayResult(sectionDiv, `There was an error: ${error}`);
+        });
+
+});
 
 
+function generateList(data) {
+    console.log(data);
+    let html = "Found " + data.total_count +" repositories about " + searchTerm;
+
+    for( let i = 0; i < data.length; i++) {
+        let item = data[i];
+        console.log(item);
+        html += "<li>" + generateListElement(item) + "</li>";
+    }
+
+    return html;
+}
+
+
+function generateListElement(data) {
+    return data.name + " - from user: " + data.owner.login
+}
 
 
 // Convenience functions
@@ -15,15 +54,9 @@
  * and configures the button inside so when it's clicked, the
  * sectionExample function will be called (with sectionContainer as an argument).
  */
-function setupExample(classSelector, sectionExample) {
+function setupSearcher(classSelector, sectionExample) {
 
     var sectionContainer = document.querySelector(classSelector);
-
-    // querySelector() can be used on any element to look for elements inside
-    // https://developer.mozilla.org/en/docs/Web/API/Element/querySelector
-    //
-    // the jQuery find() method is similar
-    // https://api.jquery.com/find/
 
     sectionContainer.querySelector("button").onclick = function() {
         sectionExample(sectionContainer);
